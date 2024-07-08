1113559873760692697bff6286ca980ea729e0f5
ao-ta <aonot06@gmail.com>
2024-07-08 09:30:07 +0900

    filter

--- api/schemas/message.py
+++ api/schemas/message.py
@@ -10,8 +10,9 @@ class MessageBase(BaseModel):
     viewingdata: str | None = Field(None, examples=["Default ViewingData"], description="Viewing data")
     star: int = Field(ge=0, le=5, description="Review Stars")
     important: bool | None = Field(False, description="Important or not")
-
-
+    # image: Base64Bytes | None = Field(None, description="Image data")
+    # image_type: str | None = Field(None, description="Image MIME type")
+    # image_filename: str | None = Field(None, description="File name of image data")
 class Message(MessageBase):
     id: int | None = Field(None, description="Message ID")
     time: datetime | None = Field(None, description="Message post time")
@@ -21,6 +22,7 @@ class Message(MessageBase):
 class System(BaseModel):
     current_id: int = Field(0, descript="Current (latest) ID")
     messages: dict[int, Message] = Field({})
+    search_word: str | None = Field(None, examples=["Keyword"], description="Message from")
 
 
 class Response(System):
